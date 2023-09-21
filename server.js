4828d087aa1b521d076a8fac360e138a10653cb4
Robert Michaud <guildwarsguy123@yahoo.com>
2023-09-21 00:27:02 -0400

    added schema

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const methodOverride = require('method-override');
+const jsxViewEngine = require('jsx-view-engine');
 const app = express();
 
-
-app.engine('jsx', jsx.create());
+// View engine
+app.engine('jsx', jsxViewEngine());
 app.set('views', './views');
 app.set('view engine', 'jsx');
 
+//Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -22,50 +23,12 @@ mongoose.connection.once('open', () => {
   console.log('Connected to MongoDB');
 });
 
-// Middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(methodOverride('_method'));
-
-const jsxViewEngine = require('jsx-view-engine');
-
-app.set('view engine', 'jsx');
-app.set('views', './views');
-app.engine('jsx', jsxViewEngine());
-
-// app.use(express.static('public'));
-
-//Index
-
 //New
 app.get('/logs/new', (req, res) => {
-  res.send('New');
+  res.render('New');
 });
 
-
-//Delete
-
-//Update
-
-//Create
-
-//Edit
-
-//Show
-app.get('/flights/:id', async (req, res) => {
-  const flight = await Flight.findById(req.params.id);
-  const id = req.params.id
-  res.render('flights/Show', { flight ,id });
-});
-
-//Post
-app.post('/flights/:id/destinations', async (req, res) => {
-  const flight = await Flight.findById(req.params.id);
-  flight.destinations.push(req.body);
-  await flight.save();
-  res.redirect(`/flights/${req.params.id}`);
-});
-
-
+// Listen on port 3000
 app.listen(3000, () => {
   console.log('Listening on port 3000');
 });
